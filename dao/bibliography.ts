879afe8b8ba679808f598f6dc852ae95eb0fa938
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-09-03 15:14:51 +0900

    🎨 Add shorthands to bibliography dao

--- dao/bibliography.ts
+++ dao/bibliography.ts
@@ -1,12 +1,12 @@
-import type { BibliographyInfoDatabase, BibliographyInfoHistoryDatabase } from '@/types/base/addresses'
-import type { BibliographyId } from '@/types/base/ids'
-import { Bibliography } from '@/types/bibliography'
-import type BibliographyModel from '@/models/bibliography'
-import { BibliographyHistory, BibliographyHistoryAction, BibliographyHistoryTarget } from '@/types/bibliography-history'
 import OrbitDB from 'orbit-db'
 import DocStore from 'orbit-db-docstore'
 import EventStore from 'orbit-db-eventstore'
 import { v4 as uuid4 } from 'uuid'
+import type { BibliographyInfoDatabase, BibliographyInfoHistoryDatabase } from '@/types/base/addresses'
+import type { BibliographyId } from '@/types/base/ids'
+import { Bibliography } from '@/types/bibliography'
+import BibliographyModel from '@/models/bibliography'
+import { BibliographyHistory, BibliographyHistoryAction, BibliographyHistoryTarget } from '@/types/bibliography-history'
 
 /**
  * This is bibliography data access object.
@@ -15,23 +15,60 @@ export default class BibliographyDao {
     #database!: DocStore<Bibliography>
     #historyDatabase!: EventStore<BibliographyHistory>
 
-    constructor (orbitdb: OrbitDB, databaseAddress: BibliographyInfoDatabase | null, historyDatabaseAddress: BibliographyInfoHistoryDatabase | null) {
+    async build (orbitdb: OrbitDB, databaseAddress: BibliographyInfoDatabase | null, historyDatabaseAddress: BibliographyInfoHistoryDatabase | null) {
       databaseAddress = databaseAddress || 'bibliography'
       historyDatabaseAddress = historyDatabaseAddress || 'bibliographyHistory'
-      orbitdb.docstore(databaseAddress).then((db) => {
-        this.#database = db as DocStore<Bibliography>
-      })
-      orbitdb.log(historyDatabaseAddress).then((db) => {
-        this.#historyDatabase = db as EventStore<BibliographyHistory>
-      })
+      this.#database = await orbitdb.docstore(databaseAddress) as DocStore<Bibliography>
+      this.#historyDatabase = await orbitdb.log(historyDatabaseAddress) as EventStore<BibliographyHistory>
     }
 
-    addBibliography (bibliography: BibliographyModel) {
-      if (this.getBibliography(bibliography.id)) {
+    createBibliography (
+      id: string,
+      name: string,
+      author: string,
+      category: string,
+      image: string,
+      publishedDate: Date,
+      publisher: string,
+      note: string,
+      issuer: string
+    ): BibliographyModel {
+      const createdDate = new Date()
+      const bibliography = new BibliographyModel(
+        id, name, author,
+        category,
+        createdDate, createdDate,
+        [],
+        image, publishedDate, publisher,
+        note, this.#historyDatabase, issuer)
+      return bibliography
+    }
+
+    getBibliographyAddress () {
+      return this.#database.address.root
+    }
+
+    getBibliographyHistoryAddress () {
+      return this.#historyDatabase.address.root
+    }
+
+    convertToModel (bibliography: Bibliography, issuer: string): BibliographyModel {
+      return new BibliographyModel(
+        bibliography.id, bibliography.name, bibliography.author,
+        bibliography.category,
+        bibliography.createdDate, bibliography.updatedDate,
+        bibliography.histories,
+        bibliography.image, bibliography.publishedDate, bibliography.publisher,
+        bibliography.note, this.#historyDatabase, issuer)
+    }
+
+    async addBibliography (bibliography: BibliographyModel) {
+      if (this.isBibliographyExist(bibliography.id)) {
         throw new Error(`Bibliography ${bibliography.id} already exists.`)
       }
-      this.#database.put(bibliography)
+      const dbPutResp = await this.#database.put(bibliography)
       this.addHistory('create', 'bibliography', bibliography.id)
+      return dbPutResp
     }
 
     editBibliography (bibliography: BibliographyModel) {
@@ -55,6 +92,10 @@ export default class BibliographyDao {
       const docs = this.#database.get(bibliographyId)
       if (docs.length === 0) { throw new Error(`Could not find bibliography ${bibliographyId}`) }
       return docs[0]
+    }
+
+    isBibliographyExist (bibliographyId: BibliographyId) : boolean {
+      return this.#database.get(bibliographyId).length > 0
     }
 
     private addHistory (action: BibliographyHistoryAction, target: BibliographyHistoryTarget, value: string) {
