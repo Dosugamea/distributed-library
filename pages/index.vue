f258ab4fad84485eff1bc3d1083af2d2653837c5
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-10-13 09:45:10 +0900

    🚧 Remove only for dev codes of index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -42,14 +42,11 @@
     <ThirdStepPage :wizard-step="wizardStep" @change-step="changeStep" />
     <FourthStepPage :wizard-step="wizardStep" @change-step="changeStep" />
     <LibraryConfigCard v-if="isLoaded" />
-    <b-input v-model="dbAddress" placeholder="DBアドレス"/>
-    <b-button @click="addElement">Test</b-button>
   </section>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import { nanoid } from 'nanoid'
 import FirstStepPage from '@/components/setup/first.vue'
 import SecondStepPage from '@/components/setup/second.vue'
 import ThirdStepPage from '@/components/setup/third.vue'
@@ -83,13 +80,7 @@ export default class IndexComponent extends Vue {
   get isLoaded () : boolean {
     return this.wizardStep === Wizard.INITIALIZED
   }
-  queryAndRender () {
-    const libraryObjects = this.$libraryDao.list()
-    console.log(libraryObjects)
-    // this.shelfName = libraryObject.name
-    // this.shelfNote = libraryObject.note
-    console.log('replicated!')
-  }
+
   async mounted () {
     const genesisLibraryInfo = clientStore.genesisLibraryInfo
     const libraryBookInfo = clientStore.libraryBookInfo
@@ -97,24 +88,13 @@ export default class IndexComponent extends Vue {
       await this.$libraryDao.build(this.$orbitdb, genesisLibraryInfo[0], genesisLibraryInfo[1])
       await this.$libraryBookDao.build(this.$orbitdb, libraryBookInfo[0], libraryBookInfo[1])
       this.wizardStep = Wizard.INITIALIZED
-      this.queryAndRender()
       alert('本棚の読み出しに成功しました')
-      this.$libraryDao.listenUpdate(this.queryAndRender)
+      this.$libraryDao.listenUpdate(() => {
+        console.log('replicated')
+        const libraryObjects = this.$libraryDao.list()
+        console.log(libraryObjects)
+      })
     }
-    console.log(this.$ipfs)
-    console.log(this.$orbitdb)
-    const tdb = await this.$orbitdb.docstore('hogehage12345')
-    await tdb.put({ _id: 'hogehoge', doc: 'all the things' })
-    let value = await tdb.get('hogehoge')
-    console.log(value)
-    await tdb.put({ _id: 'hogehoge', doc: 'other things' })
-    value = await tdb.get('hogehoge')
-    console.log(value)
-    value = await tdb.get('h')
-    console.log(value)
-    value = await tdb.get('o')
-    console.log(value)
   }
-  */
 }
 </script>
