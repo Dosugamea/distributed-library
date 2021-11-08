ebdc76ddbf5711b50126326c550c26a6816c82dc
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-11-08 19:19:01 +0900

    🚧 Inject bibliographyDao to nuxt global

--- plugins/gun.ts
+++ plugins/gun.ts
@@ -1,9 +1,12 @@
 import GUN from 'gun'
 import type { AppState } from '~/types/appState'
+import { BibliographyDao } from '@/dao/bibliography'
 
 function gunPlugin (_: any, inject: any) {
   const gunInstance = new GUN<AppState>(['http://localhost:8765/gun'])
+  const bibliographyDao = new BibliographyDao(gunInstance, 'identity-temp')
   inject('db', gunInstance)
+  inject('bibliographyDao', bibliographyDao)
 }
 
 export default gunPlugin
--- types/vue.d.ts
+++ types/vue.d.ts
@@ -1,8 +1,10 @@
 import { IGunChainReference } from 'gun/types/chain'
 import { AppState } from '~/types/appState'
+import type { BibliographyDao } from '@/dao/bibliography'
 
 declare module 'vue/types/vue' {
   interface Vue {
     $db: IGunChainReference<AppState>
+    $bibliographyDao: BibliographyDao
   }
 }
