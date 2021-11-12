2240c7593e677434724cc2560e83c5315ac29a05
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-11-12 09:45:33 +0900

    🎨 Injected userDao as global

--- dao/master.ts
+++ dao/master.ts
@@ -3,14 +3,17 @@ import type { AppState } from '@/types/appState'
 import { BibliographyDao } from '@/dao/bibliography'
 import { LibraryDao } from '@/dao/library'
 import { ReviewDao } from '@/dao/review'
+import { UserDao } from '@/dao/user'
 
 class MasterDao {
   bibliographyDao: BibliographyDao | null = null
   libraryDao: LibraryDao | null = null
   reviewDao: ReviewDao | null = null
+  userDao: UserDao | null = null
 
   initDao (peers: string[] = ['http://localhost:8765/gun'], issuer: string = 'kafuuchino') {
     const gun = new GUN<AppState>(peers)
+    this.userDao = new UserDao(gun)
     this.bibliographyDao = new BibliographyDao(gun, issuer)
     this.libraryDao = new LibraryDao(gun, issuer)
     this.reviewDao = new ReviewDao(gun, issuer)
