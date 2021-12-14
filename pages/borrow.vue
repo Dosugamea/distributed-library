56f3bb21c489f5791195d706c720cc3f30dd6b27
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-12-14 11:15:43 +0900

    ✨ Add way to borrowBook using form

--- pages/borrow.vue
+++ pages/borrow.vue
@@ -1,14 +1,44 @@
 <template>
   <section>
+    <b-loading
+      v-model="isLoading"
+      :is-full-page="true"
+      :can-cancel="false"
+    />
     <p class="title has-text-centered">
       本を借りる
     </p>
-    <form class="box mx-2 my-2 has-text-centered" @submit="onSubmit" @submit.prevent>
-      <b-field type="" label="追加する書誌" :label-position="labelPosition">
+    <div class="box mx-2 my-2 has-text-centered">
+      <b-field type="" label="本棚の選択" :label-position="labelPosition">
         <b-select
-          v-model="targetBibliographyId"
+          v-model="targets.library"
           icon="tag"
-          :placeholder="selectPlaceholder"
+          :placeholder="libraryPlaceholder"
+          :disabled="libraries.length == 0"
+          expanded
+          required
+        >
+          <option
+            v-for="l in libraries"
+            :key="l.id"
+            :value="l"
+          >
+            {{ l.name }}
+          </option>
+        </b-select>
+        <b-button
+          type="is-primary"
+          icon-left="refresh"
+          @click="loadLibraries"
+        >
+          一覧読み込み
+        </b-button>
+      </b-field>
+      <b-field type="" label="蔵書の選択" :label-position="labelPosition">
+        <b-select
+          v-model="targets.bibliography"
+          icon="tag"
+          :placeholder="bookPlaceholder"
           :disabled="filteredBibliographies.length == 0"
           expanded
           required
@@ -16,149 +46,208 @@
           <option
             v-for="b in filteredBibliographies"
             :key="b.id"
-            :value="b.id"
+            :value="b"
           >
             {{ b.name }}
           </option>
         </b-select>
+        <b-button
+          type="is-primary"
+          icon-left="refresh"
+          :disabled="targets.library === null"
+          @click="loadBibliographies"
+        >
+          一覧読み込み
+        </b-button>
         <b-input
           v-model="searchFilter"
           icon="format-title"
           type="text"
-          placeholder="絞り込みワードを入力..."
+          placeholder="絞り込みワード"
         />
       </b-field>
-      <b-field label="メモ(任意)" :label-position="labelPosition">
-        <b-input
-          v-model="targetNote"
-          icon="comment"
-          type="text"
-          placeholder="傷無し問題なし"
-          minlength="0"
-          maxlength="300"
-        />
-      </b-field>
-      <input type="submit" class="button is-primary is-large" value="本棚を追加する">
-    </form>
+      <b-button
+        label="本を借りる"
+        type="is-primary is-large"
+        :disabled="targets.library === null || targets.bibliography === null"
+        @click="checkBookStatus"
+      />
+    </div>
+    <b-modal v-model="showConfirm">
+      <form action="">
+        <div class="modal-card" style="width: auto">
+          <header class="modal-card-head">
+            <p class="modal-card-title">
+              本を借りる
+            </p>
+          </header>
+          <section class="modal-card-body">
+            <p>
+              {{ libraryName }} から
+              {{ bibliographyName }} を 借りてよろしいですか?
+            </p>
+            <p>
+              (この下の借りるを押すと 貸出記録が確定します)
+            </p>
+          </section>
+          <footer class="modal-card-foot">
+            <b-button
+              label="借りる"
+              type="is-primary"
+              @click="rentBook"
+            />
+            <b-button
+              label="キャンセル"
+              type="is-secondary"
+              @click="showConfirm = false"
+            />
+          </footer>
+        </div>
+      </form>
+    </b-modal>
   </section>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Watch } from 'vue-property-decorator'
 import { LibraryModel } from '@/models/library'
 import { BibliographyModel } from '@/models/bibliography'
 import { LibraryBookDao } from '@/dao/libraryBook'
-import { DaoWatcher, DaoWatcherState } from '@/dao/watcher'
 
 @Component({})
 export default class LibraryAddPage extends Vue {
   labelPosition = 'on-border'
-  isLoading = true
-  libraryId = this.$route.params.id
-  library: LibraryModel | null = null
-  timer: NodeJS.Timeout | null = null
-  libraryBookDao: LibraryBookDao | null = null
-  watcher: DaoWatcher<BibliographyModel> | null = null
+  showConfirm = false
+  isLoading = false
+  libraries: LibraryModel[] = []
+  bibliographies: BibliographyModel[] = []
   searchFilter: string = ''
-  targetBibliographyId: string | null = null
-  targetNote: string = ''
-
-  loadFailed () {
-    if (this.timer != null) {
-      clearTimeout(this.timer)
+  libraryBookDao: LibraryBookDao | null = null
+  targets: {
+    library: LibraryModel | null
+    bibliography: BibliographyModel | null
+  } = {
+      library: null,
+      bibliography: null
     }
+
+  loadFailed (reason: string) {
+    alert(reason)
     this.isLoading = false
-    this.$router.back()
   }
 
-  async mounted () {
+  mounted () {
     if (this.$db.libraryDao == null) {
       this.$router.push('/')
-      return
-    }
-    this.timer = setTimeout(() => {
-      alert('データ取得がタイムアウトしました')
-      this.loadFailed()
-    }, 5 * 1000)
-    if (this.$db.bibliographyDao != null) {
-      this.watcher = new DaoWatcher<BibliographyModel>(this.$db.bibliographyDao)
-    }
-    try {
-      const library = await this.$db.libraryDao.get(this.libraryId)
-      this.library = library
-      this.libraryBookDao = this.$db.libraryDao.getBookDao(library)
-      this.isLoading = false
-      clearTimeout(this.timer)
-    } catch (e) {
-      alert('存在しない本棚が指定されました')
-      this.loadFailed()
     }
   }
 
-  async onSubmit () {
-    if (this.targetBibliographyId == null) {
+  get libraryPlaceholder () {
+    if (this.libraries.length === 0) {
+      return '本棚一覧を読み込んでください...'
+    }
+    return '本棚を選択してください...'
+  }
+
+  get bookPlaceholder () {
+    if (this.targets.library === null) {
+      return '本棚を選択してください...'
+    }
+    if (this.bibliographies.length === 0) {
+      return '蔵書一覧を読み込んでください...'
+    }
+    if (this.targets.bibliography == null) {
+      return '蔵書を選択してください...'
+    }
+    return 'そのワードが含まれる蔵書はありません'
+  }
+
+  loadLibraries () {
+    if (this.$db.libraryDao == null) {
       return
     }
-    if (this.library == null) {
+    this.libraries = this.$db.libraryDao.list()
+  }
+
+  @Watch('targets.library')
+  watchLibraryChange () {
+    if (this.$db.libraryDao == null || this.targets.library == null) {
       return
     }
+    this.libraryBookDao = this.$db.libraryDao.getBookDao(this.targets.library)
+    this.bibliographies = []
+    this.targets.bibliography = null
+  }
+
+  loadBibliographies () {
     if (this.libraryBookDao == null) {
       return
     }
-    const bibliography = await this.$db.bibliographyDao!.get(this.targetBibliographyId)
-    if (bibliography == null) {
+    this.bibliographies = this.libraryBookDao.listBookAsBibliography()
+    if (this.bibliographies.length !== 0) {
+      this.targets.bibliography = this.bibliographies[0]
+    } else {
       this.$buefy.snackbar.open({
-        message: '指定された書誌IDが見つかりませんでした(削除された可能性があります)',
-        type: 'is-danger'
+        message: 'この本棚には蔵書が登録されていません',
+        type: 'is-warning'
       })
-      return
     }
-    const newModel = this.libraryBookDao!.createModel(
-      bibliography,
-      this.library,
-      this.targetNote
-    )
-    const existedBook = this.libraryBookDao!.findBookByBibliographyId(bibliography.id)
-    if (existedBook != null) {
-      this.$buefy.snackbar.open({
-        message: 'この書誌は既に追加済みです',
-        type: 'is-danger'
-      })
-      return
-    }
-    await this.libraryBookDao!.add(newModel, bibliography, this.library)
-    this.$buefy.snackbar.open({
-      message: '登録に成功しました',
-      type: 'is-success'
-    })
-    this.$router.push(`/library/${this.libraryId}`)
-  }
-
-  get bibliographies () {
-    return DaoWatcherState.elements
   }
 
   get filteredBibliographies () {
-    if (this.watcher == null) {
+    if (this.bibliographies.length === 0) {
       return []
     }
-    return DaoWatcherState.elements.filter(
-      e => e.name.includes(this.searchFilter)
-    )
+    if (this.searchFilter === '') {
+      return this.bibliographies
+    }
+    return this.bibliographies.filter((b) => {
+      return b.name.includes(this.searchFilter)
+    })
   }
 
-  get selectPlaceholder () {
-    if (this.filteredBibliographies.length > 0) {
-      return '書誌を選択...'
-    } else {
-      return 'そのワードが含まれる書誌はありません'
+  checkBookStatus () {
+    if (this.libraryBookDao == null || this.targets.library == null || this.targets.bibliography == null) {
+      return
     }
+    const book = this.libraryBookDao.findBookByBibliographyId(this.targets.bibliography.id)
+    if (book == null) {
+      alert('不正な蔵書データが指定されました')
+      return
+    }
+    if (book.rentable === false) {
+      alert('指定された蔵書は貸出中です')
+      return
+    }
+    this.showConfirm = true
   }
 
-  beforeDestroy () {
-    if (this.watcher != null) {
-      this.watcher.destroy()
+  get libraryName () {
+    return this.targets.library === null ? '' : this.targets.library.name
+  }
+
+  get bibliographyName () {
+    return this.targets.bibliography === null ? '' : this.targets.bibliography.name
+  }
+
+  async rentBook () {
+    if (this.libraryBookDao == null || this.targets.library == null || this.targets.bibliography == null) {
+      return
     }
+    this.isLoading = true
+    const book = this.libraryBookDao.findBookByBibliographyId(this.targets.bibliography.id)
+    if (book == null) {
+      this.loadFailed('不正な蔵書データが指定されました')
+      return
+    }
+    if (book.rentable === false) {
+      this.loadFailed('蔵書は貸出中です')
+      return
+    }
+    await this.libraryBookDao.borrowBook(book)
+    this.isLoading = false
+    alert(`${this.libraryName}から${this.bibliographyName}を借りました`)
+    this.$router.back()
   }
 }
 </script>
